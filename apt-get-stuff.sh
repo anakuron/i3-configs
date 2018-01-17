80d294a3508c6043d4e2571d5162233ad456770e
anakuron <antti.nissinen@gmail.com>
2018-01-17 22:35:35 +0200

    added playerctl to apt-get script

--- apt-get-stuff.sh
+++ apt-get-stuff.sh
@@ -2,6 +2,12 @@ read -p "Install packages with apt-get? Y/N " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 sudo apt-get install i3blocks fonts-font-awesome blueman compton conky libxml2-utils scrot rxvt-unicode i3 feh openvpn resolvconf evtest mosh net-tools
-echo "SCRIPT all done"
+echo "apt-get done"
 fi
-
+read -p "Install playerctrl for i3blocks Y/N " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+wget https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb && sudo dpkg -i playerctl-0.5.0_amd64.deb
+echo "playerctrl"
+fi
+echo "all done!"

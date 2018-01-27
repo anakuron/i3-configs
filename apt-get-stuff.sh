678a891daa258e2c9f0d3d201a1938c5c576528b
anakuron <antti.nissinen@gmail.com>
2018-01-28 01:49:24 +0200

    added xbindkeys

--- apt-get-stuff.sh
+++ apt-get-stuff.sh
@@ -1,7 +1,7 @@
 read -p "Install packages with apt-get? Y/N " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-sudo apt-get install i3blocks fonts-font-awesome blueman compton conky libxml2-utils scrot rxvt-unicode i3 feh openvpn resolvconf evtest mosh net-tools
+sudo apt-get install i3blocks fonts-font-awesome blueman compton conky libxml2-utils scrot rxvt-unicode i3 feh openvpn resolvconf evtest mosh net-tools mpv xbindkeys
 echo "apt-get done"
 fi
 read -p "Install playerctrl for i3blocks Y/N " -n 1 -r

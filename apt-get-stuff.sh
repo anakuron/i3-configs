cc5d5d1a7af5a76b0cdbf53540c3e81775ed707f
anakuron <antti.nissinen@gmail.com>
2018-02-08 21:08:26 +0200

    updates for i3 config and apt script

--- apt-get-stuff.sh
+++ apt-get-stuff.sh
@@ -2,7 +2,7 @@
 read -p "Install packages with apt-get? Y/N " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-sudo apt-get install i3blocks fonts-font-awesome blueman compton conky libxml2-utils scrot rxvt-unicode i3 feh openvpn resolvconf evtest mosh net-tools mpv xbindkeys lm-sensors libnotify-bin
+sudo apt-get install i3blocks fonts-font-awesome blueman compton conky libxml2-utils scrot rxvt-unicode i3 feh openvpn resolvconf evtest mosh net-tools mpv xbindkeys lm-sensors libnotify-bin bucklespring
 echo "apt-get installations done"
 fi
 read -p "Remove packages for i3 stuff? Y/N " -n 1 -r
